4b6504bb0f10ef180992b99313b39537dc1ba3fe
logonrm <logonrm@fiap.com.br>
2017-09-19 22:20:59 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const curso = require('./app/curso');
+const app = require('express')();
+
+
+app.get("/", function(req, res){
+
+});
+
+app.listen(3000, function(){
+
+});
---
+++ app/curso/index.js
@@ -0,0 +1,3 @@
+exports.dao = require('./curso.dao/');
+exports.model = require('./curso.model/');
+exports.route = require('./curso.route/');

72fefcf5da457419bfcdd51af412e3368b4af64e
logonrm <logonrm@fiap.com.br>
2017-10-05 22:30:05 -0300

    correcao packege.json url relativa

--- config/server.js
+++ config/server.js
@@ -28,9 +28,9 @@ app.use(expressValidator());
 
 /* efetua o autoload das rotas, dos models e dos controllers para o objeto app */
 consign()
-	.include('app/routes')
-	.then('app/models')
-	.then('app/controllers')
+	.include('./app/routes')
+	.then('./app/models')
+	.then('./app/controllers')
 	.into(app);
 
 /* exportar o objeto app */
